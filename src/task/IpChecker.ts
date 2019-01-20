f130c6da0169b62299fe749e303014d7868f25e0
champgm <github@champgm.e4ward.com>
2019-01-19 21:53:05 -0500

    WOrking, a little

--- src/task/IpChecker.ts
+++ src/task/IpChecker.ts
@@ -1,50 +1,92 @@
 import { Configuration } from 'configuration';
 import ping from 'ping';
+import Twilio from 'twilio';
+import { enumerateError } from '../common/ObjectUtil';
+import { promisify } from 'util';
+const pingSync = promisify(ping.sys.)
 
 export interface State {
   running: boolean;
 }
 
 export class IpCheckerTask {
-  private static fiveMinutes = 300000;
-  // private static tenSeconds = 10000;
+  // private static interval = 300000;
+  private static interval = 10000;
   public state: State;
-  private networkDevicesToCheck: { name: string; ip: string; }[];
+  private networkDevicesToCheck: { [name: string]: string };
+  private twilioClient: Twilio.Twilio;
+  private contactNumbers: { [name: string]: string };
+  private twilioNumber: string;
+  private knownOffline: { [name: string]: boolean };
 
-  constructor(configuration:Configuration) {
+  constructor(configuration: Configuration) {
     this.state = {
       running: false,
     };
-    this.networkDevicesToCheck=configuration.networkDevicesToCheck;
+    this.networkDevicesToCheck = configuration.networkDevicesToCheck;
+    this.twilioClient = Twilio(
+      configuration.twilio.accountSid,
+      configuration.twilio.authToken,
+    );
+    this.contactNumbers = configuration.contactNumbers;
+    this.twilioNumber = configuration.twilio.number;
+    this.knownOffline = {};
   }
 
   public start() {
     this.state.running = true;
-    console.log(`Running IChecker`);
-
-    this.networkDevicesToCheck.forEach((device)=>{
-      const ip = device.ip;
-      ping.sys.probe(ip,  (isAlive)=>{
-        if(!isAlive){
-// SEND EMAIL
-        }else{
-          console.log(`Network device, '${ip}' is alive`);
+    Object.keys(this.networkDevicesToCheck).forEach((deviceName) => {
+      const ip = this.networkDevicesToCheck[deviceName];
+      ping.sys.probe(ip, (isAlive) => {
+        if (!isAlive) {
+          if (!this.knownOffline[deviceName]) {
+            Object.values(this.contactNumbers).forEach(async (number) => {
+              await this.sendSms(deviceName, number, true);
+            });
+          } else {
+            console.log(`Network device, '${deviceName}' is still offline`);
+          }
+        } else {
+          if (this.knownOffline[deviceName]) {
+            Object.values(this.contactNumbers).forEach(async (number) => {
+              await this.sendSms(deviceName, number, false);
+            });
+          } else {
+            console.log(`Network device, '${deviceName}' is still online`);
+          }
         }
       });
     });
-
     this.state.running = false;
     this.restart();
   }
 
   public restart() {
     if (!this.state.running) {
-      console.log(`Restarting IpChecker`);
       setTimeout(() => {
         this.start();
-      },         IpCheckerTask.fiveMinutes);
+      }, IpCheckerTask.interval);
     } else {
       console.log(`IpChecker already running, will not restart`);
+    }
+  }
+
+  public async sendSms(deviceName: string, number: string, online: boolean) {
+    try {
+      const body = online
+        ? `The device, '${deviceName}' is back online!`
+        : `The device, '${deviceName}' is offline!`;
+      console.log(body);
+      console.log(`Sending SMS...`);
+      await this.twilioClient.messages.create({
+        body,
+        to: number,
+        from: this.twilioNumber,
+      });
+      this.knownOffline[deviceName] = true;
+    } catch (error) {
+      console.log(`Error ocurred while sending Twilio SMS`);
+      console.log(`${JSON.stringify(enumerateError(error), null, 2)}`);
     }
   }
 }
